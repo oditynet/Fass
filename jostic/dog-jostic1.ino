f1ee63d0ecfb4827eedffa1373eb40eaf9f8e1be
oditynet <oditynet@gmail.com>
2025-06-27 09:36:04 +0300

    Add files via upload

--- jostic/dog-jostic1.ino
+++ jostic/dog-jostic1.ino
@@ -1,25 +1,29 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <EncButton.h>
 
 // Конфигурация NRF24L01
-#define CE_PIN 7
+#define CE_PIN   7
 #define CSN_PIN 8
-RF24 radio(CE_PIN, CSN_PIN);
-const byte address[6] = "ROBOT";
+const byte slaveAddress[4] = {'F','A','S','S'};
+RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
 
 // Пины джойстиков
-const int LEFT_JOYSTICK_X = A0;
-const int LEFT_JOYSTICK_Y = A1;
-const int RIGHT_JOYSTICK_X = A2;
-const int RIGHT_JOYSTICK_Y = A3;
+const int LEFT_JOYSTICK_X = A2;
+const int LEFT_JOYSTICK_Y = A3;
+const int RIGHT_JOYSTICK_X = A0;
+const int RIGHT_JOYSTICK_Y = A1;
 
 struct RemoteData {
   int leftX;
   int leftY;
   int rightX;
   int rightY;
+  int btn1;
+  int btn2;
+  int btn3;
+  int btn4;
+  int checksum; // Добавляем поле для контрольной суммы
 };
 
 // Параметры калибровки
@@ -27,17 +31,14 @@ const int NEUTRAL_ZONE = 50;  // Зона нечувствительности
 const int MIN_THRESHOLD = 400; // Нижний порог активации
 const int MAX_THRESHOLD = 600; // Верхний порог активации
 
-Button btn1(3);
-Button btn2(2);
-Button btn3(6);
-Button btn4(5);
-
-// Светодиод статуса
-//const int STATUS_LED = 4;
+const int button1 = 3;
+const int button2 = 2;
+const int button3 = 6;
+const int button4 = 5;
 
 // Таймер для отправки данных
 unsigned long lastSendTime = 0;
-const int SEND_INTERVAL = 200; // Интервал отправки (мс)
+const int SEND_INTERVAL = 500; // Интервал отправки (мс)
 
 void setup() {
   Serial.begin(115200);
@@ -49,18 +50,22 @@ void setup() {
   pinMode(RIGHT_JOYSTICK_X, INPUT);
   pinMode(RIGHT_JOYSTICK_Y, INPUT);
 
-  radio.begin();
-  radio.setChannel(108); // Канал 108 (2.508 GHz)
-  radio.setDataRate(RF24_250KBPS); // Низкая скорость = стабильнее
-  radio.setPALevel(RF24_PA_MAX); // Макс. мощность
-  radio.setRetries(3, 5); // 3*250мкс задержка, 5 попыток
-  radio.openWritingPipe(address);
-  radio.stopListening();
+    radio.begin();
+    radio.setDataRate( RF24_250KBPS );
+    //radio.setRetries(1,1); // delay, count
+    radio.openWritingPipe(slaveAddress);
+    radio.setChannel(98);
+     radio.setAutoAck(true);
   
   while (!radio.isChipConnected()) {
     Serial.println("Ошибка: nRF24L01 не подключен!");
     delay(1000);
   }
+  
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(button3, INPUT_PULLUP);
+  pinMode(button4, INPUT_PULLUP);
 
   Serial.println("Пульт готов к работе");
 }
@@ -68,23 +73,6 @@ void setup() {
 void loop() {
   // Чтение значений с джойстиков
   RemoteData data;
-
-  btn1.tick();
-  btn2.tick();
-  btn3.tick();
-  btn4.tick();
-
-  //if (btn1.click()) Serial.println("btn 1 click!");
-  if (btn1.hold()) Serial.println("btn 1!");
-  if (btn2.hold()) Serial.println("btn 2!");
-  if (btn3.hold()) Serial.println("btn 3!");
-  if (btn4.hold()) Serial.println("btn 4!");
-  /*SensorData data = {
-    analogRead(A0),
-    analogRead(A1),
-    analogRead(A2),
-    analogRead(A3)
-  };*/
   
   // Отправка данных с заданным интервалом
   if (millis() - lastSendTime >= SEND_INTERVAL) {
@@ -92,9 +80,27 @@ void loop() {
     data.leftY = analogRead(LEFT_JOYSTICK_Y);
     data.rightX = analogRead(RIGHT_JOYSTICK_X);
     data.rightY = analogRead(RIGHT_JOYSTICK_Y);
+    
+
+    data.btn1 = !digitalRead(button1); // Инвертируем, так как INPUT_PULLUP
+    data.btn2 = !digitalRead(button2);
+    data.btn3 = !digitalRead(button3);
+    data.btn4 = !digitalRead(button4);
 
     applyCalibration(data);
-    sendData(data);
+
+    // Вычисление контрольной суммы
+    data.checksum = data.leftX +data.leftY +data.rightX +data.rightY +data.btn1+data.btn2+data.btn3+data.btn4;
+
+    bool result = radio.write(&data, sizeof(data));
+    if (result) {
+        Serial.println("[+]");
+    }
+    else {
+        Serial.print("-");
+    }
+
+
     lastSendTime = millis();
   }
   
@@ -104,7 +110,7 @@ void loop() {
     printJoystickData(data);
     lastPrintTime = millis();
   }
-  delay(100);
+  delay(300);
 }
 
 // Применение калибровки и зоны нечувствительности
@@ -125,12 +131,6 @@ int mapAndFilter(int value) {
     return 512; // Нейтральное положение
   }
   return value;
-}
-
-// Отправка данных роботу
-void sendData(RemoteData data) {
-  bool result = radio.write(&data, sizeof(data));
-  //Serial.print(result ? "[+]" : "[-]");
 }
 
 // Вывод данных джойстиков в Serial
